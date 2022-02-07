ad3529957cf9181f4799af42f3fedfde7108ffb4
Andy McCoy <andy.k.mccoy@gmail.com>
2022-02-06 20:32:41 -0800

    remove --silent

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'Fetching photon-db...';
-curl -J "http://graphhopper-data-proxy-svc.$POD_NAMESPACE.svc.cluster.local/download1_graphhopper$DOWNLOADS_GRAPHHOPPER_PATH" -o $DOWNLOADS_GRAPHHOPPER_FILE_NAME --silent
+curl -J "http://graphhopper-data-proxy-svc.$POD_NAMESPACE.svc.cluster.local/download1_graphhopper$DOWNLOADS_GRAPHHOPPER_PATH" -o $DOWNLOADS_GRAPHHOPPER_FILE_NAME
 echo 'Finished downloading photon-db.';
 
 echo 'Decompressing photon-db...';
