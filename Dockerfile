8afb69e7de9c119be73ebcdf3ee0c0bf465a4adf
Andy McCoy <andy.k.mccoy@gmail.com>
2022-02-06 21:46:56 -0800

    update main.sh

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN mkdir -p /usr/app
 # copy startup script
 COPY ./main.sh /usr/app
 
+WORKDIR /usr/app
+
 # get photon jar file
 RUN curl -LJ https://github.com/komoot/photon/releases/download/0.3.5/photon-0.3.5.jar -o /usr/app/photon-0.3.5.jar --silent
 
--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo 'Fetching photon-db...';
+echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] Fetching photon-db...";
 curl -J "http://graphhopper-data-proxy-svc.$POD_NAMESPACE.svc.cluster.local/download1_graphhopper$DOWNLOADS_GRAPHHOPPER_PATH" -o $DOWNLOADS_GRAPHHOPPER_FILE_NAME --silent
-echo 'Finished downloading photon-db.';
+echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] Finished downloading photon-db.";
 
-echo 'Decompressing photon-db...';
+echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] Decompressing photon-db...";
 bzip2 -cd $DOWNLOADS_GRAPHHOPPER_FILE_NAME | tar x
-echo 'Finished decompressing photon-db.';
+echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] Finished decompressing photon-db.";
 
 java -jar ./photon-*.jar
