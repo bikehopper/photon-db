52fd554fb4f1de573e6a6bb6a15f83d98ef12779
Andy McCoy <andy.k.mccoy@gmail.com>
2022-03-05 13:30:24 -0800

    logging

--- build.sh
+++ build.sh
@@ -7,5 +7,8 @@ touch ./photon_data/test1.txt
 echo 'Hello Andy' >> ./photon_data/test1.txt
 
 tar -zvcf "photon_data.tar.gz" ./photon_data
-
-minio-upload.sh $BUCKET_NAME /elasticsearch photon_data.tar.gz
+echo 'ls /bin...'
+ls /bin
+echo 'ls ./...'
+ls -halt
+./minio-upload.sh $BUCKET_NAME /elasticsearch photon_data.tar.gz
