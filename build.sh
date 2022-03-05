2234b5ceb4b3b7e141c0a5924086709f2206db5e
Andy McCoy <andy.k.mccoy@gmail.com>
2022-03-05 13:09:11 -0800

    add some logging

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # java -jar ./photon-*.jar -nominatim-import -host nominatim-app-svc.$POD_NAMESPACE -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
+pwd
+ls -halt
+
 mkdir -p ./photon_data
 touch ./photon_data/test1.txt
 echo 'Hello Andy' >> ./photon_data/test1.txt
