047d75d90cda0a2fd6ebc1f3b6c10aa288e19342
Andy McCoy <andy.k.mccoy@gmail.com>
2022-03-05 14:15:14 -0800

    bash debug

--- minio-upload.sh
+++ minio-upload.sh
@@ -11,13 +11,22 @@ host="${MINIO_HOST}.${POD_NAMESPACE}"
 s3_key=$ACCESS_KEY
 s3_secret=$SECRET_KEY
 
+echo "host: ${host}"
+echo "s3_key: ${s3_key}"
+echo "s3_secret: ${s3_secret}"
+
 resource="/${bucket}${prefix}/${file_path}"
 content_type="application/octet-stream"
 date=`date -R`
 _signature="PUT\n\n${content_type}\n${date}\n${resource}"
 signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
 
-curl --fail-with-body -X PUT -T "${file_path}" \
+echo "host: ${host}"
+echo "s3_key: ${s3_key}"
+echo "s3_secret: ${s3_secret}"
+echo "resource: ${resource}"
+
+curl --silent -v --fail-with-body -X PUT -T "${file_path}" \
           -H "Host: ${host}" \
           -H "Date: ${date}" \
           -H "Content-Type: ${content_type}" \
