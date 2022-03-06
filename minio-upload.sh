86be3683c5f2eb5df0bcff069c2de6f5d18f2c19
Andy McCoy <andy.k.mccoy@gmail.com>
2022-03-05 17:18:17 -0800

    still trying to get logs

--- minio-upload.sh
+++ minio-upload.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-set -eu
+# set -eu
 # Usage: ./minio-upload my-bucket /prefix my-file_path.zip
 
 bucket=$1
@@ -11,25 +11,17 @@ host="${MINIO_HOST}.${POD_NAMESPACE}"
 s3_key=$ACCESS_KEY
 s3_secret=$SECRET_KEY
 
-echo "host: ${host}"
-echo "s3_key: ${s3_key}"
-echo "s3_secret: ${s3_secret}"
-
 resource="/${bucket}${prefix}/${file_path}"
 content_type="application/octet-stream"
 date=`date -R`
 _signature="PUT\n\n${content_type}\n${date}\n${resource}"
 signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
 
-echo "host: ${host}"
-echo "s3_key: ${s3_key}"
-echo "s3_secret: ${s3_secret}"
-echo "resource: ${resource}"
+curl --silent -v --fail-with-body -o output -X PUT -T "${file_path}" \
+  -H "Host: ${host}" \
+  -H "Date: ${date}" \
+  -H "Content-Type: ${content_type}" \
+  -H "Authorization: AWS ${s3_key}:${signature}" \
+  http://${host}${resource}
 
-# curl --silent -v --fail-with-body -X PUT -T "${file_path}" \
-curl -v -X PUT -T "${file_path}" \
-          -H "Host: ${host}" \
-          -H "Date: ${date}" \
-          -H "Content-Type: ${content_type}" \
-          -H "Authorization: AWS ${s3_key}:${signature}" \
-          http://${host}${resource}
+cat output
