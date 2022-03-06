0268f23de49dcbb6d28a6cf1cecdabb2a05be7fe
Andy McCoy <andy.k.mccoy@gmail.com>
2022-03-05 18:25:05 -0800

    move to aws cli from curl

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -eu
-# java -jar ./photon-*.jar -nominatim-import -host nominatim-app-svc.$POD_NAMESPACE -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
 
-mkdir -p ./photon_data
-touch ./photon_data/test1.txt
-echo 'Hello Andy' >> ./photon_data/test1.txt
+java -jar ./photon-*.jar -nominatim-import -host nominatim-app-svc.$POD_NAMESPACE -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
 
 tar -zvcf "photon_data.tar.gz" ./photon_data
 
--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./minio-download.sh $BUCKET_NAME /elasticsearch/photon_data.tar.gz ./photon_data.tar.gz
+AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url http://$MINIO_HOST.$POD_NAMESPACE --bucket $BUCKET_NAME --key elasticsearch/photon_data.tar.gz photon_data.tar.gz
 
-tar -xvzf photon_data.tar.gz -C ./photon_data
+tar -xvzf photon_data.tar.gz
 
 java -jar ./photon-*.jar
