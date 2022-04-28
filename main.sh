6c0ffd32f3abc0da8fb915515020b5bc339edf5f
Andy McCoy <andy.k.mccoy@gmail.com>
2022-04-27 22:00:20 -0700

    point all to staging

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
-AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url http://$MINIO_HOST.$POD_NAMESPACE --bucket $BUCKET_NAME --key elasticsearch/photon_data.tar.gz photon_data.tar.gz
+AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url http://$MINIO_HOST.staging --bucket $BUCKET_NAME --key elasticsearch/photon_data.tar.gz photon_data.tar.gz
 
 tar -xvzf photon_data.tar.gz
 
-java -jar ./photon-*.jar -host nominatim-app-svc.$POD_NAMESPACE -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
+java -jar ./photon-*.jar -host nominatim-app-svc.staging -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
