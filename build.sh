27d5c77f3e6bc70428d21643625e96d1d1652fcc
Andy McCoy <andy.k.mccoy@gmail.com>
2022-11-18 20:40:19 -0800

    update shell scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-java -jar ./photon-*.jar -nominatim-import -host nominatim-app-svc.staging -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
+java -jar ./photon-*.jar -nominatim-import -host nominatim-app-svc.$POD_NAMESPACE -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
 
 tar -zvcf "photon_data.tar.gz" ./photon_data
 
-AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api put-object --endpoint-url http://$MINIO_HOST.staging --bucket $BUCKET_NAME --key elasticsearch/photon_data.tar.gz --body photon_data.tar.gz
+AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api put-object --endpoint-url $MINIO_HOST --bucket $BUCKET_NAME --key elasticsearch/photon_data.tar.gz --body photon_data.tar.gz
--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url http://$MINIO_HOST.staging --bucket $BUCKET_NAME --key elasticsearch/photon_data.tar.gz photon_data.tar.gz
+AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url $MINIO_HOST --bucket $BUCKET_NAME --key elasticsearch/photon_data.tar.gz photon_data.tar.gz
 
 tar -xvzf photon_data.tar.gz
 
