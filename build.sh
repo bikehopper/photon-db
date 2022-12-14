4abd9204f63d3da9c46965b06dcde0f860ad9134
Andy McCoy <andy.k.mccoy@gmail.com>
2022-12-14 01:30:33 -0800

    remove manual tar balling

--- build.sh
+++ build.sh
@@ -2,5 +2,3 @@
 set -eu
 
 java -jar ./photon-*.jar -nominatim-import -host nominatim-app-svc.$POD_NAMESPACE -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
-
-tar --use-compress-program="pigz -k -9" -zcf "photon_data.tar.gz" ./photon_data
