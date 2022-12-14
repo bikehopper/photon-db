212651152b5c0ba3ded7b70a933614d1b7b441d9
Andy McCoy <andy.k.mccoy@gmail.com>
2022-12-13 23:28:08 -0800

    remove manual tarring

--- build.sh
+++ build.sh
@@ -4,5 +4,3 @@ set -eu
 java -jar ./photon-*.jar -nominatim-import -host nominatim-app-svc.$POD_NAMESPACE -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
 
 tar -zvcf "photon_data.tar.gz" ./photon_data
-
-AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api put-object --endpoint-url $MINIO_HOST --bucket $BUCKET_NAME --key elasticsearch/photon_data.tar.gz --body photon_data.tar.gz
