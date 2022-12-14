de2a753ec18c19f7d33bf7c0b13f05cb4bc1ef77
Andy McCoy <andy.k.mccoy@gmail.com>
2022-12-14 01:57:22 -0800

    tar.gz to tgz

--- main.sh
+++ main.sh
@@ -3,6 +3,6 @@ set -e
 
 AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url $MINIO_HOST --bucket $BUCKET_NAME --key elasticsearch/photon_data.tgz photon_data.tgz
 
-tar -xzf photon_data.tar.gz
+tar -xzf photon_data.tgz
 
 java -jar ./photon-*.jar
