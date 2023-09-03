7e0a339d866c62d998f9bcf9621cbad7dc98b04b
Andy McCoy <andy.k.mccoy@gmail.com>
2023-09-03 12:33:30 -0700

    make photon data download optional

--- main.sh
+++ main.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 
-AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url $MINIO_HOST --bucket $BUCKET_NAME --key elasticsearch/photon_data.tgz photon_data.tgz
+if [ -z "$(ls -A ./photon_data | grep -v .gitkeep)" ]; then # if empty photon_data dir
+  echo "Downloading photon_data tarball..."
+  AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url $MINIO_HOST --bucket $BUCKET_NAME --key elasticsearch/photon_data.tgz photon_data.tgz
+  echo "Downloaded photon_data tarball."
+  echo "Decompressing photon_data tarball..."
+  tar -xzf photon_data.tgz
+  echo "Decompressed photon_data tarball."
+fi
 
-tar -xzf photon_data.tgz
-
+echo "Starting photon..."
 java -jar ./photon-*.jar
