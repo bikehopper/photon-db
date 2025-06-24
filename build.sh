15921a922a456aaa9be2333168b1bb0a0fffcc0f
Andy McCoy <andy.k.mccoy@gmail.com>
2025-06-24 15:46:16 -0700

    update to photon opensearch v0.7.0

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-exec java -jar ./photon-*.jar -nominatim-import -host nominatim-app-svc.$POD_NAMESPACE -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
+exec java -jar ./photon-*.jar -nominatim-import -host $NOMINATIM_HOST -port $NOMINATIM_PORT -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -z "$(ls -A ./photon_data | grep -v .gitkeep)" ]; then # if empty photon_data dir
   echo "Downloading photon_data tarball..."
-  AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url $MINIO_HOST --bucket $BUCKET_NAME --key elasticsearch/photon_data.tgz photon_data.tgz
+  AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url $MINIO_HOST --bucket $BUCKET_NAME --key $PHOTON_DATABASE_KEY photon_data.tgz
   echo "Downloaded photon_data tarball."
   echo "Decompressing photon_data tarball..."
   tar -xzf photon_data.tgz
