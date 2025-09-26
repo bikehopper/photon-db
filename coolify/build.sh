64b333146110d731886568fffa606c4c6db622f3
Arindam Bose <arindam.1993@gmail.com>
2025-09-26 16:08:05 -0700

    coolify scripts

---
+++ coolify/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -eu
+
+# -host param is setup as a network alias for the nominatim service in coolify
+exec java -jar photon-*.jar -nominatim-import -host nominatim.db -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
