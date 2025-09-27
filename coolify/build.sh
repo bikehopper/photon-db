65fcffe9352fe72a141cea4b76c9e58a6ad97315
Arindam Bose <arindam.1993@gmail.com>
2025-09-27 10:27:19 -0700

    move everything to start phase

--- coolify/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-
-# -host param is setup as a network alias for the nominatim service in coolify
-exec java -jar photon-*.jar -nominatim-import -host local.nominatim.db -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
--- coolify/run.sh
+++ coolify/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+java -jar photon-*.jar -nominatim-import -host local.nominatim.db -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
 java -jar photon-*.jar
