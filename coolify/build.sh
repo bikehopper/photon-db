e1aa1bed69dcd687c728245e1157285993c00207
Arindam Bose <arindam.1993@gmail.com>
2025-09-27 00:12:53 -0700

    switch to global domain

--- coolify/build.sh
+++ coolify/build.sh
@@ -2,4 +2,4 @@
 set -eu
 
 # -host param is setup as a network alias for the nominatim service in coolify
-exec java -jar photon-*.jar -nominatim-import -host host.docker.internal -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
+exec java -jar photon-*.jar -nominatim-import -host nominatim.cool-bikehopper.org -port 5432 -database nominatim -user nominatim -password $NOMINATIM_PASSWORD
